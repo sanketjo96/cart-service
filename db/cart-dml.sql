ca92436c160f15fed310c8c4aa7762b6de78600b
EPAM\Sanket_Joshi <sanket_joshi@epam.com>
2023-05-03 21:35:00 +0530

    init

---
+++ db/cart-dml.sql
@@ -0,0 +1,19 @@
+-- Carts table filling
+insert into carts (user_id, created_at, updated_at, status) values
+(uuid_generate_v4(), '2023-03-30', '2023-03-31', 'ORDERED'),
+(uuid_generate_v4(), '2023-03-31', '2023-04-01', 'OPEN'),
+(uuid_generate_v4(), '2023-04-01', '2023-04-01', 'OPEN');
+
+-- Cart items table filling
+-- Note: product_id should be taken on existing DB: DynamoDB or PostgreSQL depending on which in use right now.
+insert into cart_items (cart_id, product_id, count) values
+('1e2a8d17-890d-407d-aef0-14bc31cb0832', uuid_generate_v4(), 1),
+('2252102e-4dba-4eb4-bdb6-99133764fd4c', uuid_generate_v4(), 10),
+('b2b9d29a-5c13-451f-9548-2ea0d7d5c123', uuid_generate_v4(), 2),
+('798c8eea-00be-4e16-875c-0b49ea21e376', uuid_generate_v4(), 3);
+
+-- All Carts selection
+SELECT * FROM carts
+
+-- All Cart items selection
+SELECT * FROM cart_items
